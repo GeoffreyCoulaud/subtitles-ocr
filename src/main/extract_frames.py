98687814e8c74175f1d80f5fcb1c186213023c85
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2025-07-08 00:17:17 +0200

    Add print messages

--- src/main/extract_frames.py
+++ src/main/extract_frames.py
@@ -11,11 +11,19 @@ from pathlib import Path
 import subprocess
 from tempfile import TemporaryDirectory
 from fractions import Fraction
+from shutil import move
 
 
 class Arguments(Namespace):
     video_path: Path
     output_dir: Path
+
+
+def print_banner(message: str):
+    """Print a banner with the given message."""
+    print("-" * 20)
+    print(message)
+    print("-" * 20)
 
 
 def frame_number_to_timestamp(frame_number: int, fps: float) -> str:
@@ -48,6 +56,8 @@ def main():
     with TemporaryDirectory() as temp_dir:
         temp_path = Path(temp_dir)
 
+        print_banner(f"Extracting frames from {args.video_path}")
+
         # Execute the command
         # ffmpeg -vsync 0 -i video.avi -f image2 -frame_pts 1 %d.png
         command = [
@@ -64,6 +74,8 @@ def main():
         ]
         subprocess.run(command, check=True)
 
+        print_banner("Renaming frames")
+
         # Get the video's frame rate
         # ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 video.avi
         command = [
@@ -79,18 +91,28 @@ def main():
             str(args.video_path),
         ]
         raw_fps = subprocess.run(command, capture_output=True, text=True, check=True)
-
-        # Parse the frame rate
         fps = float(Fraction(raw_fps.stdout.strip()))
 
         # Rename frames to include the frame number and timestamp
         # In the format: frame_<frame_number>_00:00:00,000.png
         # Move frames to the output directory
-        for frame in temp_path.iterdir():
+        frames = list(temp_path.iterdir())
+        for i, frame in enumerate(frames):
+
+            # Print progress (only print the first, the last and every 1% frame)
+            if i == 0 or i == len(frames) - 1 or i % (len(frames) // 100) == 0:
+                percent = round(i / len(frames) * 100)
+                print(f"[{percent: 3}%] Renaming {frame.name}...")
+
             frame_number = int(frame.stem)
             frame_timestamp = frame_number_to_timestamp(frame_number, fps)
             new_name = f"{frame_number}_{frame_timestamp}.png"
-            frame.rename(args.output_dir / new_name)
+
+            # We cannot use `frame.rename()` here because it does not work across different filesystems.
+            # Tempororary dirs are often created on tmpfs (RAM disk).
+            move(src=frame, dst=args.output_dir / new_name)
+
+        print_banner(f"Done! Frames saved to {args.output_dir}")
 
 
 if __name__ == "__main__":
